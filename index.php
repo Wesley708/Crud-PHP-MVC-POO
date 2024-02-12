9afacd79d386201342ae6b9db58dfec2af86f64c
Wesley708 <65147671+Wesley708@users.noreply.github.com>
2024-02-11 21:14:06 -0300

    Utilizando o primeiro paconte baixado como o composer

--- index.php
+++ index.php
@@ -1,5 +1,12 @@
 <?php
+    require_once "vendor/autoload.php";
 
-    echo 'Olá mundo!';
+    use Cocur\Slugify\Slugify;
+
+    $slug = new Slugify();
+    echo $slug->slugify('Teste com acentução para verificar a criação de um slug!','_');
+
+    $slug->addRule('ão', 'seila');
+    echo '</br>' . $slug->slugify('Teste com acentução para verificar a criação de um slug!','_');
 
 ?>
